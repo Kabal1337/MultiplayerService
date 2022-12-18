4152f84accef36edd7ffe371f33b7aa10afea13e
Kabal1337 <boronkabal@gmail.com>
2022-12-18 22:35:32 +0300

    множественное подключение пользователей и синхронизация

--- MultiplayerClient/Program.cs
+++ MultiplayerClient/Program.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
+using System.IO;
 
 namespace MultiplayerClient
 {
@@ -10,6 +11,7 @@ namespace MultiplayerClient
         // адрес и порт сервера, к которому будем подключаться
         static int port = 8005; // порт сервера
         static string address = "127.0.0.1"; // адрес сервера
+        
         static void Main(string[] args)
         {
             try
@@ -19,25 +21,31 @@ namespace MultiplayerClient
                 Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
                 // подключаемся к удаленному хосту
                 socket.Connect(ipPoint);
-                Console.Write("Введите сообщение:");
-                string message = Console.ReadLine();
-                byte[] data = Encoding.Unicode.GetBytes(message);
-                socket.Send(data); 
-                data = new byte[256]; // буфер для ответа
-                StringBuilder builder = new StringBuilder();
-                int bytes = 0; // количество полученных байт
+                //Console.Write("Введите сообщение:");
+                StreamReader stream = new StreamReader("ld.json");
+                while (true)
+                { 
+                    
+                    string message = stream.ReadToEnd();
+                    stream.BaseStream.Position=0;
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    socket.Send(data);
+                    data = new byte[256]; // буфер для ответа
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0; // количество полученных байт
 
-                do
-                {
-                    bytes = socket.Receive(data, data.Length, 0);
-                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    do
+                    {
+                        bytes = socket.Receive(data, data.Length, 0);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (socket.Available > 0);
+                    Console.WriteLine("ответ сервера: " + builder.ToString());
                 }
-                while (socket.Available > 0);
-                Console.WriteLine("ответ сервера: " + builder.ToString());
-
                 // закрываем сокет
-                socket.Shutdown(SocketShutdown.Both);
-                socket.Close();
+                //socket.Shutdown(SocketShutdown.Both);
+                //socket.Close();
+                //Console.WriteLine("SocketClosed");
             }
             catch (Exception ex)
             {
--- MultiplayerServ/Server.cs
+++ MultiplayerServ/Server.cs
@@ -1,15 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MultiplayerServ
 {
     class Server
     {
         static int port = 8005; //порт для приема входящих запросов
+        List<string> messages_list = new List<string>();
+        
         static void Main(string[] args)
         {
+            
             //адрес локальной точки, с которой будет связан сокет
             IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
 
@@ -20,18 +25,41 @@ namespace MultiplayerServ
             {
                 //свзять сокета с локальной точкой
                 listenSocket.Bind(ipPoint);
-
+                List<Socket> socket_list = new List<Socket>();
                 //начало прослушивания подключений
                 listenSocket.Listen(10); //10 - максимальная длина очереди подключений
                 Console.WriteLine("Сервер запущен. Ожидание подключений...");
                 while (true)
                 {
                     //получение сообщения
-                    Socket handler = listenSocket.Accept();
+                    Socket handler_tmp = listenSocket.Accept();
+                    if (handler_tmp != null)
+                    {
+                        int socket_num = socket_list.Count;
+                        Task task = Task.Factory.StartNew(() => SocketDataHandler(handler_tmp, socket_list, socket_num)); //запуск отдельным потоком обработку сокета.
+                        socket_list.Add(handler_tmp);
+                        
+                        //if (handler_tmp != null)
+                        //socket_list.Add(handler_tmp);
+                        //Socket handler = handler_tmp;
+                    }
+                    Console.WriteLine("Число подключений: " + socket_list.Count);
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            void SocketDataHandler(Socket handler, List<Socket> socket_list, int socket_num)
+            {
+                while (true)
+                {
                     StringBuilder builder = new StringBuilder();
                     int bytes = 0; //кол-во полученныйх байт
                     byte[] data = new byte[256]; //буфер данных
-                    //сборка сообщения
+                                                 //сборка сообщения
                     do
                     {
                         bytes = handler.Receive(data);
@@ -40,21 +68,21 @@ namespace MultiplayerServ
                     while (handler.Available > 0);
                     Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
                     // отправляем ответ
-                    string message = "ваше сообщение доставлено";
-                    data = Encoding.Unicode.GetBytes(message);
-                    handler.Send(data);
+                    //string message = "ваше сообщение доставлено";
+                    data = Encoding.Unicode.GetBytes(builder.ToString());
+                    for (int i = 0; i < socket_list.Count; i++)
+                    {
+                        if (i != socket_num) socket_list[i].Send(data);
+                    }
+                    //handler.Send(data);
                     // закрываем сокет
-                    handler.Shutdown(SocketShutdown.Both);
-                    handler.Close();
+                    //handler.Shutdown(SocketShutdown.Both);
+                    //handler.Close();
                 }
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-
 
         }
+
+        
     }
 }
